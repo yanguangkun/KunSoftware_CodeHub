c83c0b59b60246045f5fd1e04719dd09ce0716bb
yanguangkun <yanguangkun@126.com>
2013-11-01 17:36:45 +0800

    修改web目录

--- src/main/java/cn/codehub/App.java
+++ src/main/java/cn/codehub/App.java
@@ -10,4 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    
+    public static String getName() {
+    	return "bbss";
+    }
 }
--- src/test/java/cn/codehub/AppTest.java
+++ src/test/java/cn/codehub/AppTest.java
@@ -26,6 +26,7 @@ public class AppTest
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
+        
     }
 
     /**
